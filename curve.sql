3233a77f2ef6b7b3349dcde29b47615d7ebbfee7
Brian Shaginaw <bshaginaw120@gmail.com>
2019-10-19 17:24:52 -0400

    Label sample curves

--- curve.sql
+++ curve.sql
@@ -1,21 +1,25 @@
--- SELECT create_svgpath('M 100 350 q 150 -300 300 0', 5, bytea '\x0000FF');
-
+-- === QUADRATIC BEZIER CURVE ===
 -- SELECT encode(to_ppm(
 --     450, 400,
 --     ARRAY[
---         create_svgpath('M 100 350 q 150 -300 300 0 Z', 5, bytea '\x0000FF')
+--         create_svgpath('M 100 350 q 150 -300 300 0', 5, bytea '\x0000FF')
 --     ]
 -- ), 'base64');
 
--- SELECT encode(to_ppm(
---     200, 100,
---     ARRAY[
---         create_svgpath('M 10,50
---            Q 25,25 40,50
---            t 30,0 30,0 30,0 30,0 30,0', 5, bytea '\x0000FF')
---     ]
--- ), 'base64');
+-- === SIN WAVES ===
+SELECT encode(to_ppm(
+    200, 200,
+    ARRAY[
+        create_svgpath('M 10,50
+           Q 25,25 40,50
+           t 30,0 30,0 30,0 30,0 30,0', 5, bytea '\x0000FF'),
+        create_svgpath('M 10, 150
+            q 15 25 30 0
+            t 30,0 30,0 30,0 30,0 30,0', 5, bytea '\xFF0000')
+    ]
+), 'base64');
 
+-- === FIRST PERIOD OF A SIN WAVE ===
 -- SELECT encode(to_ppm(
 --     200, 100,
 --     ARRAY[
@@ -25,13 +29,10 @@
 --     ]
 -- ), 'base64');
 
-SELECT encode(to_ppm(
-    450, 400,
-    ARRAY[
-        create_svgpath('M 100 350 L 150 300 200 325 Z', 5, bytea '\x0000FF')
-    ]
-), 'base64');
-
--- SELECT bezier_points(100, 350, 250, 50, 400, 350);
-
--- SELECT create_svgpath('M 100 350 q 150 -300 300 0', 5, bytea '\x0000FF') @> point (;
+-- === TRIANGLE ===
+-- SELECT encode(to_ppm(
+--     450, 400,
+--     ARRAY[
+--         create_svgpath('M 100 350 L 150 300 200 325 Z', 5, bytea '\x0000FF')
+--     ]
+-- ), 'base64');
